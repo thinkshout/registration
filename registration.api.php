1032ba4329cfeda7c7605788e70c50dc4e1382ee
Lev Tsypin <lev@levelos.com>
2012-04-10 12:52:24 -0700

    Adding hook_registration_entity_access() to api.

--- registration.api.php
+++ registration.api.php
@@ -19,3 +19,22 @@ function hook_registration_access($op, $registration = NULL, $account = NULL, $e
     return TRUE;
   }
 }
+
+/**
+ * Provide a form API element exposed as a Registration enttity setting.
+ *
+ * @param array $settings
+ *   Existing settings values.
+ *
+ * @return array
+ */
+function hook_registration_entity_settings($settings) {
+  return array(
+    'registration_entity_access_roles' => array(
+      '#type' => 'checkboxes',
+      '#title' => t('Roles'),
+      '#description' => t('Override default access control settings by selecting which roles can register for this event.'),
+      '#options' => user_roles(),
+      '#default_value' => isset($settings['settings']['registration_entity_access_roles']) ? $settings['settings']['registration_entity_access_roles'] : NULL
+  ));
+}
